75a196f9731743709370519844142e7b8b898e3e
ARuslanDenisov <angess47@gmail.com>
2024-06-10 14:22:58 +0700

    [FEAT]: did MainView, add some animations, add 2 images and 4 colors in assets

--- EpicRPS/Screens/MainView.swift
+++ EpicRPS/Screens/MainView.swift
@@ -7,9 +7,116 @@
 import SwiftUI
 
 struct MainView: View {
+    @State private var firstAnim = false
+    @State private var secondAnim = false
     var body: some View {
-        Color(.rpsLightGray)
-            .ignoresSafeArea()
+        VStack {
+            HStack {
+                // Settings button
+                Button(action: {
+                    //someAction
+                }, label: {
+                    Image("settings-icon")
+                })
+                Spacer()
+                // Help button
+                Button(action: {
+                    //someAction
+                }, label: {
+                    Image("help-icon")
+                })
+            }
+            .padding(18)
+            Spacer()
+            VStack {
+                HStack {
+                    Spacer()
+                    Image("male-rock-cut")
+                        .resizableToFit()
+                        .offset(x: secondAnim ? 0 : 200)
+                        .scaleEffect(secondAnim ? 1.2 : 0.8)
+                }
+                
+                
+                Text("EPIC RPS")
+                    .font(.custom("Rubik-Bold", fixedSize: 30))
+                    .foregroundStyle(.rpsLightPeach)
+                    .shadow(color: .rpsDarkPeach, radius: 0, x: 2, y: 2)
+                    .padding(.vertical, 50)
+                    .scaleEffect(firstAnim ? 1 : 0 )
+                
+                HStack {
+                    Image("female-scissors-cut")
+                        .resizableToFit()
+                        .offset(x: secondAnim ? 0 : -200)
+                        .scaleEffect(secondAnim ? 1.2 : 0.8)
+                    Spacer()
+                }
+            }
+            
+            .frame(maxWidth: .infinity, maxHeight: 310)
+            .padding(.vertical, 30)
+            
+            VStack(spacing: 20){
+                //Start button
+                Button{
+                    //some action
+                } label: {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 25)
+                            .foregroundStyle(.rpsDarkPeach)
+                            .shadow(color: .rpsShadowPeach, radius: 0, y: 6)
+                        HStack {
+                            RoundedRectangle(cornerRadius: 25)
+                                .foregroundStyle(.rpsLightPeach)
+                            Spacer(minLength: 100)
+                        }
+                        Text("START")
+                            .font(.custom("Rubik-Bold", fixedSize: 16))
+                            .foregroundStyle(.rpsPeachText)
+                    }
+                    .frame(maxHeight: 54)
+                    
+                    
+                }
+                // Result button
+                Button{
+                    //some action
+                } label: {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 25)
+                            .foregroundStyle(.rpsDarkPeach)
+                            .shadow(color: .rpsShadowPeach, radius: 0, y: 6)
+                        HStack {
+                            RoundedRectangle(cornerRadius: 25)
+                                .foregroundStyle(.rpsLightPeach)
+                            Spacer(minLength: 100)
+                        }
+                        Text("RESULT")
+                            .font(.custom("Rubik-Bold", fixedSize: 16))
+                            .foregroundStyle(.rpsPeachText)
+                    }
+                    .frame(maxHeight: 54)
+                    
+                }
+            }
+            .frame(maxWidth: 200, maxHeight: 200)
+            
+        }
+        
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(.rpsLightGray))
+        
+        .onAppear{
+            firstAnim.toggle()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0) {
+                secondAnim.toggle()
+
+                       }
+            
+        }
+        .animation(.bouncy(duration: 1, extraBounce: 0.3), value: firstAnim)
+        .animation(.easeOut(duration: 1).delay(0.05), value: secondAnim)
     }
 }
 
