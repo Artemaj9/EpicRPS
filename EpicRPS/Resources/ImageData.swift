8ba4100ab3c8fcc6503eb9b9328e21c4ce73f1cd
ArtemMoiseev <moiseev@physics.msu.ru>
2024-06-11 23:21:37 +0300

    [FIX]: fix visibility of assets and SettingsButton

---
+++ EpicRPS/Resources/ImageData.swift
@@ -0,0 +1,15 @@
+//
+//  ImageData.swift
+//  EpicRPS
+//
+//  Created by Daria on 11.06.2024.
+//
+
+import Foundation
+
+enum RulesImage {
+    static var paper = "Paper"
+    static var scissors = "Scissors"
+    static var stone = "Stone"
+    static var yellowCircle = "YellowCircle"
+}
--- EpicRPS/Resources/TextData.swift
+++ EpicRPS/Resources/TextData.swift
@@ -8,7 +8,14 @@ import Foundation
  только для примера, для компактности большие тексты желательно выносить из вью.
  */
 enum RulesTxt {
+    static let rule = "Rules"
     static let rule1 = "Игра проводится между игроком и комьютером."
+    static let rule2 = "Жесты:"
+    static let rule21 = "Кулак > Ножницы"
+    static let rule22 = "Бумага > Кулак"
+    static let rule23 = "Ножницы > Бумага"
+    static let rule3 = "У игрока есть 30 сек. для выбора жеста."
+    static let rule4 = "Игра ведётся до трёх побед одного из участников."
 }
 
 // MARK: - arms pic names
