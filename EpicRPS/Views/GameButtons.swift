32097487ab6d53ff1e4fdbd7cfcede27f9889d66
Evgeniy Kukarskiy <ekukarskiy@gmail.com>
2024-06-11 11:05:18 +0500

    added GameButtons pictures, GameButtons view file

---
+++ EpicRPS/Views/GameButtons.swift
@@ -0,0 +1,18 @@
+//
+//  GameButtons.swift
+//  EpicRPS
+//
+//  Created by Evgeniy K on 11.06.2024.
+//
+
+import SwiftUI
+
+struct GameButtons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameButtons()
+}
