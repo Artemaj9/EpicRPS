3980a8f0418ce4960ddfe5fd9001744176a9d000
Korotkova Daria <Dkuvashova@mail.ru>
2024-06-11 20:41:02 +0300

    [FEAT] - Add avatars to the project

---
+++ EpicRPS/Resources/TextImage.swift
@@ -0,0 +1,20 @@
+//
+//  TextImage.swift
+//  EpicRPS
+//
+//  Created by Daria on 11.06.2024.
+//
+
+import Foundation
+
+enum CharacterAvatarImage {
+    static let charAvatar1 = "character1"
+    static let charAvatar2 = "character2"
+    static let charAvatar3 = "character3"
+    static let charAvatar4 = "character4"
+    static let charAvatar5 = "character5"
+    static let charAvatar6 = "character6"
+    static let charAvatar7 = "character7"
+    static let charAvatar8 = "character8"
+}
+
