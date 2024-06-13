1129e2a0ac9aa79985cf1e6deb3ed938d145f906
ARuslanDenisov <angess47@gmail.com>
2024-06-13 22:36:28 +0700

    [FEAT] 67 Leader_View

--- EpicRPS/Screens/LeaderBoardView.swift
+++ EpicRPS/Screens/LeaderBoardView.swift
@@ -5,17 +5,153 @@
 import SwiftUI
 
 struct LeaderBoardView: View {
-    var body: some View {
-        VStack {
-            NavigationHeader(title: "Leaderboard")
-                .frame(maxWidth: .infinity)
-            Spacer()
-            
+    @EnvironmentObject var vm: GameViewModel
+    var players2: [Player] {
+        vm.allPlayers.sorted {
+            $0.wins > $1.wins
         }
-        .navigationBarHidden(true)
+    }
+    
+    var playersTest = [Player(name: "Andrei", games: 3002, wins: 21, avatar: "avatarTest"),
+                       Player(name: "Masha", games: 302, wins: 11, avatar: "avatarTest"),
+                       Player(name: "Kate", games: 342, wins: 21, avatar: "avatarTest"),
+                       Player(name: "Irina", games: 102, wins: 31, avatar: "avatarTest"),
+                       Player(name: "Matew", games: 402, wins: 41, avatar: "avatarTest"),
+                       Player(name: "Floa", games: 2402, wins: 51, avatar: "avatarTest"),
+                       Player(name: "Inira", games: 12, wins: 11, avatar: "avatarTest"),
+                       Player(name: "Russsasdsdna", games: 902, wins: 71, avatar: "avatarTest"),
+                       Player(name: "Kksas", games: 3002, wins: 61, avatar: "avatarTest"),
+                       Player(name: "Asksa", games: 3002, wins: 81, avatar: "avatarTest"),
+                       Player(name: "Know", games: 3002, wins: 91, avatar: "avatarTest"),
+                       Player(name: "Mew_mew_newsss", games: 3002, wins: 0, avatar: "avatarTest"),
+    ]
+    var players: [Player] {
+        playersTest.sorted {
+            $0.wins > $1.wins
+        }
+    }
+    @State private var showName = false
+    
+    var body: some View {
+        ZStack {
+            
+            VStack {
+                NavigationHeader(title: "Leaderboard")
+                    .frame(maxWidth: .infinity)
+                HStack {
+                    Image(vm.currentPlayer1.avatar)
+                        .resizableToFit()
+                    HStack{
+                        Button {
+                            showName.toggle()
+                        } label: {
+                            Text(vm.currentPlayer1.name)
+                                .font(.custom(.rubikMedium, size: 20))
+                                .foregroundStyle(.rpsGray)
+                                .padding(13)
+                        }
+                        
+                        .padding(.horizontal, 10)
+                        
+                        Spacer()
+                    }
+                    .frame(width: 249)
+                    .background(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(.rpsGray.opacity(0.2), lineWidth: 1)
+                    }
+                    Spacer()
+                    
+                }
+                .frame(height: 46)
+                .padding(.horizontal, 20)
+                VStack {
+                    ZStack {
+                        VStack {
+                            Color(.white)
+                                .padding(2)
+                                .background(.white)
+                                .clipShape(RoundedRectangle(cornerRadius: 45))
+                                .offset(y: 30)
+                                .padding(2)
+                        }
+                        Rectangle()
+                            .foregroundColor(.white)
+                            .offset(y: 90)
+                            .padding(2)
+                        VStack {
+                            Image("topImage")
+                                .resizableToFit()
+                                .frame(height: 95)
+                            Spacer()
+                        }
+                        VStack {
+                            Spacer()
+                            HStack {
+                                Text("Игроки")
+                                    .font(.custom(.rubikMedium, size: 15))
+                                    .foregroundStyle(.rpsDefaultTextForCell)
+                                Spacer()
+                                Text("Очки")
+                                    .font(.custom(.rubikMedium, size: 15))
+                                    .foregroundStyle(.rpsDefaultTextForCell)
+                                Spacer()
+                                Text("Рейтинг")
+                                    .font(.custom(.rubikMedium, size: 15))
+                                    .foregroundStyle(.rpsDefaultTextForCell)
+                            }
+                        }
+                        .padding(.horizontal, 35)
+                        
+                    }
+                    .frame(height: 130)
+                    
+                    ScrollView {
+                        
+                        ForEach (players2.count == 0 ? players.indices: players2.indices) { ind in
+                            let player = players[ind]
+                            let points = players[ind].wins * 500
+                            let rate = players[ind].wins
+                            LeaderBoardCell(placeInRankin: ( ind + 1 ), avatar: player.avatar, name: player.name, result: points, rate: rate)
+                            
+                            if ind > 3 {
+                                VStack{
+                                    Rectangle()
+                                        .foregroundStyle(.rpsTextForCell.opacity(0.3))
+                                        .frame(width: 250, height: 0.3)
+                                    Spacer()
+                                }
+                            }
+                        }
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(.white)
+                    
+                }
+                .animation(.easeInOut(duration: 1), value: showName)
+                
+            }
+            .blur(radius: showName ? 5 : 0)
+            
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.rpsLightGray)
+            .navigationBarHidden(true)
+            if showName {
+                NameEditingView(check: $showName)
+                    .environmentObject(vm)
+                    .shadow(radius: 20)
+            }
+                
+        }
+
     }
 }
 
 #Preview {
     LeaderBoardView()
+        .environmentObject(GameViewModel())
 }
+
+
--- EpicRPS/Views/NameEditingView.swift
+++ EpicRPS/Views/NameEditingView.swift
@@ -14,52 +14,63 @@ struct NameEditingView: View {
     var checkLetters: Bool {
         name.count <= 2
     }
+    @Binding var check: Bool
     var body: some View {
-        VStack {
-            HStack {
-                Text("Введите имя игрока")
-                    .font(.custom(.rubikRegular, size: 17))
-                    .foregroundStyle(.rpsGray)
-                Spacer()
-            }
-            TextField("Player1", text: $name)
-                .font(.custom(.rubikMedium, size: 23))
-                .padding(15)
-                .background(.rpsLightGray)
-                .clipShape(RoundedRectangle(cornerRadius: 30))
-                .overlay {
-                    RoundedRectangle(cornerRadius: 30)
-                        .stroke(.gray, lineWidth: 1)
+        ZStack {
+            Rectangle()
+                .foregroundStyle(.black)
+                .opacity(0.01)
+                .onTapGesture {
+                    check.toggle()
+                }
+            VStack {
+                
+                HStack {
+                    Text("Введите имя игрока")
+                        .font(.custom(.rubikRegular, size: 16))
+                        .foregroundStyle(.rpsGray)
+                    Spacer()
+                }
+                TextField("Player1", text: $name)
+                    .font(.custom(.rubikMedium, size: 23))
+                    .padding(15)
+                    .background(.rpsLightGray)
+                    .clipShape(RoundedRectangle(cornerRadius: 30))
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 30)
+                            .stroke(.gray, lineWidth: 1)
                         
+                    }
+                Spacer()
+                Button {
+                    vm.currentPlayer1.name = name
+                    UserDefaultsService.shared.save(structs: vm.currentPlayer1, forKey: "currentPlayer1")
+                    dismiss()
+                    check.toggle()
+                }label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 30)
+                            .foregroundStyle(.rpsShadowPeach)
+                        Text(checkLetters ? "Еще \(3 - name.count) \(name.count == 2 ? "буква" : "буквы" )" : checkBigLetters ? "Много букв": "OK")
+                            .font(.custom(.rubikMedium, size: 17))
+                            .foregroundStyle(.rpsLightGray)
+                    }
+                    .frame(width: 136, height: 37)
                 }
-            Spacer()
-            Button {
-                vm.currentPlayer1.name = name
-                UserDefaultsService.shared.save(structs: vm.currentPlayer1, forKey: "currentPlayer1")
-                dismiss()
-            }label: {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 30)
-                        .foregroundStyle(.rpsShadowPeach)
-                    Text(checkLetters ? "Еще \(3 - name.count) \(name.count == 2 ? "буква" : "буквы" )" : checkBigLetters ? "Много букв": "OK")
-                        .font(.custom(.rubikMedium, size: 17))
-                        .foregroundStyle(.rpsLightGray)
-                }
-                .frame(width: 136, height: 37)
+                .disabled(checkLetters || checkBigLetters)
+                .saturation(checkLetters || checkBigLetters ? 0 : 1)
             }
-            .disabled(checkLetters || checkBigLetters)
-            .saturation(checkLetters || checkBigLetters ? 0 : 1)
+            .padding(20)
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .frame(width: 314, height: 196)
+            .animation(.easeInOut, value: checkLetters)
+            .animation(.easeInOut, value: checkBigLetters)
         }
-        .padding(10)
-        .background(.white)
-        .clipShape(RoundedRectangle(cornerRadius: 20))
-        .frame(width: 314, height: 196)
-        .animation(.easeInOut, value: checkLetters)
-        .animation(.easeInOut, value: checkBigLetters)
     }
 }
 
-#Preview {
-    NameEditingView()
-        .environmentObject(GameViewModel())
-}
+//#Preview {
+//    NameEditingView()
+//        .environmentObject(GameViewModel())
+//}
