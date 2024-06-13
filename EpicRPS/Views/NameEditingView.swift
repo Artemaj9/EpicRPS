0f90d9c562459f145b72f8c3c7ae86579e5396f7
ARuslanDenisov <angess47@gmail.com>
2024-06-13 14:29:56 +0700

    [FEAT] 28 nameView with animation and logic

--- EpicRPS/Views/NameEditingView.swift
+++ EpicRPS/Views/NameEditingView.swift
@@ -5,11 +5,61 @@
 import SwiftUI
 
 struct NameEditingView: View {
+    @EnvironmentObject var vm: GameViewModel
+    @Environment (\.dismiss) var dismiss
+    @State var name = "Player1"
+    var checkBigLetters: Bool {
+        name.count > 16
+    }
+    var checkLetters: Bool {
+        name.count <= 2
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Text("Введите имя игрока")
+                    .font(.custom(.rubikRegular, size: 17))
+                    .foregroundStyle(.rpsGray)
+                Spacer()
+            }
+            TextField("Player1", text: $name)
+                .font(.custom(.rubikMedium, size: 23))
+                .padding(15)
+                .background(.rpsLightGray)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .overlay {
+                    RoundedRectangle(cornerRadius: 30)
+                        .stroke(.gray, lineWidth: 1)
+                        
+                }
+            Spacer()
+            Button {
+                vm.currentPlayer1.name = name
+                UserDefaultsService.shared.save(structs: vm.currentPlayer1, forKey: "currentPlayer1")
+                dismiss()
+            }label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 30)
+                        .foregroundStyle(.rpsShadowPeach)
+                    Text(checkLetters ? "Еще \(3 - name.count) \(name.count == 2 ? "буква" : "буквы" )" : checkBigLetters ? "Много букв": "OK")
+                        .font(.custom(.rubikMedium, size: 17))
+                        .foregroundStyle(.rpsLightGray)
+                }
+                .frame(width: 136, height: 37)
+            }
+            .disabled(checkLetters || checkBigLetters)
+            .saturation(checkLetters || checkBigLetters ? 0 : 1)
+        }
+        .padding(10)
+        .background(.white)
+        .clipShape(RoundedRectangle(cornerRadius: 20))
+        .frame(width: 314, height: 196)
+        .animation(.easeInOut, value: checkLetters)
+        .animation(.easeInOut, value: checkBigLetters)
     }
 }
 
 #Preview {
     NameEditingView()
+        .environmentObject(GameViewModel())
 }
